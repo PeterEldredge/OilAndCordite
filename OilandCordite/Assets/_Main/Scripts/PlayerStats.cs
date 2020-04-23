4d4c8a3de1f8750398d99a5893d77b600453bc41
Richard Kang <Rk649@drexel.edu>
2020-04-22 21:29:08 -0400

    Added Health & Heat UI elements as well as my own testing scene

---
+++ OilandCordite/Assets/_Main/Scripts/PlayerStats.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    static public float health = 100;
+    static public float heat = 0;
+    static public bool invincible = false;
+    public static void setHealth(float newHealth)
+    {
+        health = newHealth;
+    }
+    public static void changeHealth(float change)
+    {
+        health += change;
+    }
+    public static void setHeat(float newHeat)
+    {
+        heat = newHeat;
+    }
+    public static void changeHeat(float change)
+    {
+        heat += change;
+    }
+    void OnCollisionStay(Collision collision)
+    {
+        if (!invincible)
+        {
+            changeHealth(-10);
+            StartCoroutine(invincibleTimer());
+        }
+    }
+    IEnumerator invincibleTimer()
+    {
+        invincible = true;
+        yield return new WaitForSecondsRealtime(.5f);
+        invincible = false;
+    }
+}
---
+++ OilandCordite/Assets/_Main/Scripts/UI/UITextUpdate.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITextUpdate : MonoBehaviour
+{
+    [SerializeField] private Text HealthText;
+    [SerializeField] private Text HeatText;
+    [SerializeField] private Slider HealthBar;
+    [SerializeField] private Slider HeatBar;
+
+    // Update is called once per frame
+    void Update()
+    {
+        generateHeat();
+        updateHealth();
+        updateHeat();
+    }
+    void generateHeat()
+    {
+        if (Input.GetMouseButton(0)&&PlayerStats.heat<99.9)
+        {
+            PlayerStats.changeHeat(.1f);
+        }
+        else if (PlayerStats.heat>.1)
+        {
+            PlayerStats.changeHeat(-.1f);
+        }
+    }
+    void updateHealth()
+    {
+        HealthText.text = "Health:"+PlayerStats.health.ToString("F0");
+        HealthBar.value = PlayerStats.health;
+    }
+    void updateHeat()
+    {
+        HeatText.text = "Heat:" + PlayerStats.heat.ToString("F1");
+        HeatBar.value = PlayerStats.heat;
+    }
+}
