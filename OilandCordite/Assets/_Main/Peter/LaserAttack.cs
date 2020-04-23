4a9193b893531fa6eb087d9407464820013bb035
PeterEldredge <peldredge98@gmail.com>
2020-04-22 21:17:16 -0400

    Enemy Atack Base

---
+++ OilandCordite/Assets/_Main/Peter/LaserAttack.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "LaserAttack")]
+public class LaserAttack : AttackBehaviour
+{
+    public override bool UsageCondition(PlayerData playerData, EnemyData enemyData)
+    {
+        return base.UsageCondition(playerData, enemyData);
+    }
+
+    public override void Attack()
+    {
+        Debug.LogError("In Range");
+    }
+}
