5223af32977c937c00b9a57675711fbef6386f25
PeterEldredge <peldredge98@gmail.com>
2020-04-30 00:19:41 -0400

    Added Mouse Control scheme

--- OilandCordite/Assets/_Main/Jake/Scripts/ShipControlBasic.cs
+++ OilandCordite/Assets/_Main/Jake/Scripts/ShipControlBasic.cs
@@ -1,66 +1,124 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ShipControlBasic : MonoBehaviour
 {
-
     [Header("Physics")]
     [Tooltip("Force to push plane forwards with")] public float igniteThrust = 200f;
     [Tooltip("Pitch, Yaw, Roll")] public Vector3 turnTorque = new Vector3(60f, 25f, 45f);
     [Tooltip("Multiplier for all forces")] public float rotateMult = 3f;
     [Tooltip("Increase gravity")] public float gravMult = 3.0f;
-    public float maxAcceleration = 150;
-    public float minAcceleration = -125;
+    [SerializeField] private float _maxAcceleration = 150;
+    [SerializeField] private float _minAcceleration = -125;
+    [SerializeField] private AnimationCurve _positiveAccelerationCurve;
+    [SerializeField] private AnimationCurve _negativeAccelerationCurve;
 
     [Header("Options")]
     [Tooltip("Invert the pitching of the ship to avoid \"normal\" plane controls")] public bool invertY = false;
 
     [Header("Input")]
+    [SerializeField] private bool _startingInversion = false;
+    [SerializeField] private bool _mouseMovement = false;
     [SerializeField] [Range(-1f, 1f)] private float _pitch = 0f;
     [SerializeField] [Range(-1f, 1f)] private float _roll = 0f;
 
+    [Header("Mouse Input")]
+    [SerializeField] private float _deadzone = .1f;
+    [SerializeField] private float _xSensitivity = .4f;
+    [SerializeField] private float _ySensitivity = .6f;
+
     //States
+    private Rigidbody _rb;
+
+    private Action _inputCalculation;
+
     private bool _igniting = false;
     private bool _overheating = false;
 
-    private Rigidbody rb;
-    [SerializeField] private AnimationCurve _positiveAccelerationCurve;
-    [SerializeField] private AnimationCurve _negativeAccelerationCurve;
+    private int _invertYControl;
 
-    //-1 or 1, depending on whether invertY is true or false
-    private int _invertYControl = 1;
+    private float _xMousePosition;
+    private float _yMousePosition;
 
-    // Start is called before the first frame update
-    void Start()
+    #region Input Calculations 
+        
+    private void SetInputType()
     {
-        rb = GetComponent<Rigidbody>();
-        QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 60;
+        ResetVirtualJoystick();
+
+        if (_mouseMovement) _inputCalculation = MouseCalculation;
+        else _inputCalculation = KeyboardCalculation;
     }
 
-    private void FixedUpdate()
+    private void ResetVirtualJoystick()
+    {
+        _xMousePosition = 0;
+        _yMousePosition = 0;
+    }
+
+    private void MouseCalculation()
+    {
+        _xMousePosition += Input.GetAxisRaw("Mouse X") * Time.deltaTime * _xSensitivity;
+        _yMousePosition += Input.GetAxisRaw("Mouse Y") * Time.deltaTime * _ySensitivity * _invertYControl;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ResetVirtualJoystick();
+        }
+
+        if (Mathf.Abs(_xMousePosition) > _deadzone) _roll = (Mathf.Abs(_xMousePosition) - _deadzone) * Mathf.Sign(_xMousePosition);
+        else _roll = 0;
+
+        if (Mathf.Abs(_yMousePosition) > _deadzone) _pitch = (Mathf.Abs(_yMousePosition) - _deadzone) * Mathf.Sign(_yMousePosition);
+        else _pitch = 0;
+    }
+
+    private void KeyboardCalculation()
+    {
+        _pitch = Input.GetAxis("Vertical") * _invertYControl;
+        _roll = Input.GetAxis("Horizontal");
+    }
+
+    #endregion
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
+
+        _invertYControl = _startingInversion ? 1 : -1;
+
+        SetInputType();
+    }
+
+    private void Update()
     {
         _igniting = Input.GetMouseButton(0);
 
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            if(invertY == true)
-            {
-                invertY = false;
-            }
-            else { invertY = true; }
+            _invertYControl *= -1;
         }
 
-        if (invertY) { _invertYControl = -1; }
-        else { _invertYControl = 1; }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            _mouseMovement = !_mouseMovement;
 
-        _pitch = Input.GetAxis("Vertical") * _invertYControl;
-        _roll = Input.GetAxis("Horizontal");
+            SetInputType();
+        }
 
-        transform.Rotate(new Vector3(turnTorque.x * _pitch, 0, -turnTorque.z * _roll) * rotateMult * Time.fixedDeltaTime, Space.Self);     
+        _inputCalculation.Invoke();
+    }
 
-        rb.velocity = Mathf.Clamp(rb.velocity.magnitude, 60, 300) * transform.forward;
+    private void FixedUpdate()
+    {
+        transform.Rotate(new Vector3(turnTorque.x * _pitch, 0, -turnTorque.z * _roll) * rotateMult * Time.fixedDeltaTime, Space.Self);
+
+        _rb.velocity = Mathf.Clamp(_rb.velocity.magnitude, 60, 300) * transform.forward;
 
         float forwardAngle = transform.forward.y;
         AnimationCurve accelerationCurve;
@@ -71,18 +129,18 @@ public class ShipControlBasic : MonoBehaviour
             accelerationCurve = _positiveAccelerationCurve;
             forwardAngle *= -1;
 
-            acceleration = (accelerationCurve.Evaluate(forwardAngle) * maxAcceleration);
+            acceleration = (accelerationCurve.Evaluate(forwardAngle) * _maxAcceleration);
         }
         else
         {
             accelerationCurve = _negativeAccelerationCurve;
 
-            acceleration = (accelerationCurve.Evaluate(forwardAngle) * minAcceleration);
+            acceleration = (accelerationCurve.Evaluate(forwardAngle) * _minAcceleration);
         }
 
-        rb.velocity += transform.forward * acceleration * Time.fixedDeltaTime;
+        _rb.velocity += transform.forward * acceleration * Time.fixedDeltaTime;
 
-        PlayerStats.Speed = (int)rb.velocity.magnitude;
+        PlayerStats.Speed = (int)_rb.velocity.magnitude;
 
         if (transform.position.y <= 0)
         {
@@ -97,7 +155,7 @@ public class ShipControlBasic : MonoBehaviour
         {
             if (other.tag == "Smog")
             {
-                rb.velocity += transform.forward * igniteThrust * ((PlayerStats.heat / 100) + 20) * Time.fixedDeltaTime;
+                _rb.velocity += transform.forward * igniteThrust * ((PlayerStats.heat / 100) + 20) * Time.fixedDeltaTime;
             }
         }
 
