6514e9d593de3bb0dc89aa859ddc945f7c907dd6
dixeyja <39249054+dixeyja@users.noreply.github.com>
2020-05-06 19:06:24 -0400

    Camera rotates less sharply

--- OilandCordite/Assets/_Main/Jake/Scripts/FlightCam.cs
+++ OilandCordite/Assets/_Main/Jake/Scripts/FlightCam.cs
@@ -2,28 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlightCamBasic : MonoBehaviour
+public class FlightCam : MonoBehaviour
 {
 
     public Transform ship;
     public float springBias = .96f;
-    public float distanceFromShip = 30f;
+    public float distanceFromShip = 20f;
     public float upFromShip = 15f;
     public float lookingPointFromShip = 15f;
+    public float rotateSpeed = 100f;
+    public float damping = 1f;
 
-    // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
+
         Vector3 moveCamTo = ship.position - ship.forward * distanceFromShip + Vector3.up * upFromShip;
         transform.position = transform.position * springBias + moveCamTo * (1f - springBias);
 
-        transform.LookAt(ship.position + ship.forward * lookingPointFromShip);
-
+        var newRotation = Quaternion.LookRotation((ship.position + ship.forward * lookingPointFromShip) - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * damping);
     }
 }
