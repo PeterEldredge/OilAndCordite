f9798c1ea56462b6f4bb6997d57a491b4fa55457
smcfadden150 <smcfadden150@gmail.com>
2020-05-07 14:58:50 -0400

    added clouds to demo scene, removed urp settings

---
+++ OilandCordite/Assets/_Main/Sean/Clouds/Scripts/FollowTransform.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTransform : MonoBehaviour
+{
+    public Transform followTarget;
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(followTarget.position.x, this.transform.position.y, followTarget.position.z);
+    }
+}
