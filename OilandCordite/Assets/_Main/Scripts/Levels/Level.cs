bba3b66e331e2de263e8a3932bd781e4f3e6b318
PeterEldredge <peldredge98@gmail.com>
2020-05-14 14:14:59 -0400

    Main Menu UI Updates

--- OilandCordite/Assets/_Main/Scripts/Levels/Level.cs
+++ OilandCordite/Assets/_Main/Scripts/Levels/Level.cs
@@ -7,9 +7,21 @@ using UnityEngine.UI;
 [CreateAssetMenu(menuName = "Level")]
 public class Level : ScriptableObject
 {
+    public enum LevelType
+    {
+        DestroyEnemies,
+        RaceToTheFinish,
+    }
+
     [SerializeField] private string _sceneName;
     public string SceneName => _sceneName;
 
+    [SerializeField] private LevelType _type;
+    public LevelType Type => _type;
+
     [SerializeField] private Sprite _levelSprite;
     public Sprite LevelSprite => _levelSprite;
+
+    [SerializeField, TextArea(4, 8)] private string _levelDescription;
+    public string LevelDescription => _levelDescription; 
 }
