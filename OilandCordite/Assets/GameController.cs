08eb0d6c67c9cf52a98522205272242203fad375
PeterEldredge <peldredge98@gmail.com>
2020-05-14 00:56:12 -0400

    Added mission object

---
+++ OilandCordite/Assets/GameController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : GameEventUserObject
+{
+    private void OnMissionCompleted(MissionCompleteEventArgs args)
+    {
+        SceneManager.LoadScene(0);        
+    }
+
+    public override void Subscribe()
+    {
+        EventManager.Instance.AddListener<MissionCompleteEventArgs>(this, OnMissionCompleted);
+    }
+}
---
+++ OilandCordite/Assets/_Main/Scripts/Missions/DestoyEnemiesController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoyEnemiesController : MonoBehaviour
+{
+    [SerializeField] private int _enemiesToBeKilled = 0;
+
+    private int _childrenToWin;
+
+    public void Start()
+    {
+        _childrenToWin = _enemiesToBeKilled == 0 ? 0 : transform.childCount - _enemiesToBeKilled;
+
+        StartCoroutine(CheckMissionComplete());
+    }
+
+    public IEnumerator CheckMissionComplete()
+    {
+        while(true)
+        {
+            if (transform.childCount <= _childrenToWin)
+            {
+                EventManager.Instance.TriggerEvent(new MissionCompleteEventArgs());
+
+                break;
+            }
+
+            yield return 10f;
+        }
+    }
+}
