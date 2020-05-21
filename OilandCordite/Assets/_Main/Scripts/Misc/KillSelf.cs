3d27c999bc2253b2e5737bcee11cb01cf3e1d48a
PeterEldredge <peldredge98@gmail.com>
2020-05-20 20:59:52 -0400

    Added kill self to explosion prefab

---
+++ OilandCordite/Assets/_Main/Scripts/Misc/KillSelf.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillSelf : MonoBehaviour
+{
+    [SerializeField] private float _time;
+
+    private void OnEnable()
+    {
+        Destroy(gameObject, _time);
+    }
+}
