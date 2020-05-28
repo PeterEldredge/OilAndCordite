f8862489a5c394930ebfb09c4293df148a3f67b4
Richard Kang <Rk649@drexel.edu>
2020-05-27 20:45:15 -0400

    Added the framework for Welded Weapons, and a laser welded weapon

---
+++ OilandCordite/Assets/_Main/Scripts/WeldedLaser.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeldedLaser : GameEventUserObject
+{
+    [SerializeField] private GameObject _Laser;
+    [SerializeField] private Transform _shotOrigin;
+    
+    private void useLaser(useWeldedWeaponArgs args) => useLaser();
+    private void destroy(removeWeldedWeaponArgs args) => destroy();
+    public override void Subscribe()
+    {
+        EventManager.Instance.AddListener<useWeldedWeaponArgs>(this, useLaser);
+        EventManager.Instance.AddListener<removeWeldedWeaponArgs>(this, destroy);
+    }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            useLaser();
+        }
+    }
+    private void useLaser()
+    {
+        Instantiate(_Laser,_shotOrigin.position,_shotOrigin.rotation);
+    }
+    private void destroy()
+    {
+        EventManager.Instance.RemoveListener<useWeldedWeaponArgs>(this, useLaser);
+        EventManager.Instance.RemoveListener<removeWeldedWeaponArgs>(this,destroy);
+        Destroy(gameObject);
+    }
+}
---
+++ OilandCordite/Assets/_Main/Scripts/WeldedWeapon.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum weldedWeaponTypes { EXAUST, LASER, BLADEWING };
+
+public struct WeldedWeaponPickupArgs : IGameEvent {
+    public int weaponPickup { get; }
+
+    public WeldedWeaponPickupArgs(int value)
+    {
+        this.weaponPickup = value;
+    }
+}
+public struct useWeldedWeaponArgs : IGameEvent{}
+public struct removeWeldedWeaponArgs : IGameEvent { }
+
+public class WeldedWeapon : GameEventUserObject
+{
+    [SerializeField]private GameObject[] _weldedWeapons;
+    private void AttachWeldedWeapon(WeldedWeaponPickupArgs args) => AttachWeldedWeapon(args.weaponPickup);
+    private void AttachWeldedWeapon(PlayerDefeatedEnemyEventArgs args) => AttachWeldedWeapon(0);
+    public override void Subscribe()
+    {
+        EventManager.Instance.AddListener<PlayerDefeatedEnemyEventArgs>(this, AttachWeldedWeapon);
+    }
+    private void AttachWeldedWeapon(int value)
+    {
+        GameObject obj =Instantiate(_weldedWeapons[value],gameObject.transform,false);
+    }
+}
