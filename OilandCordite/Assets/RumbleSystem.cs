21027cb1c997613031516d7ccd933537a3bd0304
PeterEldredge <peldredge98@gmail.com>
2020-05-27 21:19:56 -0400

    Rumble Updates

--- OilandCordite/Assets/RumbleSystem.cs
+++ OilandCordite/Assets/RumbleSystem.cs
@@ -4,31 +4,49 @@ using UnityEngine;
 
 public class RumbleSystem : GameEventUserObject
 {
+    [SerializeField] private AnimationCurve _heatCurve;
+    [SerializeField] private float _explosionRumbleTime;
+
     private bool _paused = false;
     private float _rumbleLevel = 0f;
-    [SerializeField] private float _speedThreshold = 300;
 
     private void OnGamePaused(Events.GamePausedEventArgs args) => _paused = true;
     private void OnGameUnpaused(Events.GameUnpausedEventArgs args) => _paused = false;
+    private void OnPlayerDefeatedEnemy(PlayerDefeatedEnemyEventArgs args) => InputHelper.Player.SetVibration(0, 1, _explosionRumbleTime);
+    private void OnPlayerDeath(PlayerDeathEventArgs args) => InputHelper.Player.SetVibration(0, 1, _explosionRumbleTime);
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+
+        StartCoroutine(RumbleRoutine());
+    }
 
     public override void Subscribe()
     {
         EventManager.Instance.AddListener<Events.GamePausedEventArgs>(this, OnGamePaused);
         EventManager.Instance.AddListener<Events.GameUnpausedEventArgs>(this, OnGameUnpaused);
+        EventManager.Instance.AddListener<PlayerDefeatedEnemyEventArgs>(this, OnPlayerDefeatedEnemy);
+        EventManager.Instance.AddListener<PlayerDeathEventArgs>(this, OnPlayerDeath);
     }
 
     public override void Unsubscribe()
     {
         EventManager.Instance.RemoveListener<Events.GamePausedEventArgs>(this, OnGamePaused);
         EventManager.Instance.RemoveListener<Events.GameUnpausedEventArgs>(this, OnGameUnpaused);
+        EventManager.Instance.RemoveListener<PlayerDefeatedEnemyEventArgs>(this, OnPlayerDefeatedEnemy);
+        EventManager.Instance.RemoveListener<PlayerDeathEventArgs>(this, OnPlayerDeath);
     }
 
-    void Update()
+    private IEnumerator RumbleRoutine()
     {
-        _rumbleLevel = _paused ? 0 : (PlayerData.Instance.Speed - _speedThreshold) / 1000;
+        while(true)
+        {
+            _rumbleLevel = _paused ? 0 : _heatCurve.Evaluate(PlayerData.Instance.Heat / 100);
 
-        InputHelper.Player.SetVibration(0, _rumbleLevel);
-        
+            InputHelper.Player.SetVibration(1, _rumbleLevel);
 
+            yield return null;
+        }
     }
 }
