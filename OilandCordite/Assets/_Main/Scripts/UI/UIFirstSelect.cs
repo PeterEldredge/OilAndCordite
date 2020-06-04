cfe84ad27004916e938bb6e788127b17747f5443
Richard Kang <Rk649@drexel.edu>
2020-06-03 21:43:06 -0400

    Main Menu should start on the Play button now

---
+++ OilandCordite/Assets/_Main/Scripts/UI/UIFirstSelect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFirstSelect : MonoBehaviour
+{
+    [SerializeField]private GameObject _FirstSelect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(_FirstSelect);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
