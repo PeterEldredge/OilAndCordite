2680bbd2201cc07320bbaa242cef0fdd3b81d4a2
dixeyja <39249054+dixeyja@users.noreply.github.com>
2020-07-06 21:01:39 -0400

    (OAC-127)  Dialog Box Concept

---
+++ OilandCordite/Assets/LookAtCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    [SerializeField] private Transform _cameraTransform;
+    [SerializeField] private float _lookSpeed = 1f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //this.transform.LookAt(_cameraTransform);
+    }
+
+    private void Update()
+    {
+        Quaternion lookRotation = Quaternion.LookRotation(_cameraTransform.position - transform.position);
+        transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, _lookSpeed * Time.deltaTime);
+    }
+}
