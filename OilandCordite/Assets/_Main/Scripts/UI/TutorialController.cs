234679afb990a6d4135d36a2c68b106b5bf980ae
Richard Kang <Rk649@drexel.edu>
2020-07-12 17:00:40 -0400

    (OAC - 132) Added tutorial popups and triggers

---
+++ OilandCordite/Assets/_Main/Scripts/UI/TutorialController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace Events
+{
+    public struct TutorialArgs : IGameEvent {
+        public string Text { get; }
+        public TutorialArgs(string text)
+        {
+            Text = text;
+        }
+    }
+}
+
+public class TutorialController : GameEventUserObject
+{
+    private AudioCuePlayer _acp;
+    [SerializeField]private GameObject _textContainer;
+    private TextMeshProUGUI _text;
+
+    private void PopUpTutorial(Events.TutorialArgs args) => PopUpTutorial(args.Text);
+    public override void Subscribe()
+    {
+        EventManager.Instance.AddListener<Events.TutorialArgs>(this, PopUpTutorial);
+    }
+    void Start()
+    {
+        _acp = gameObject.GetComponent<AudioCuePlayer>();
+        _text = _textContainer.GetComponent<TextMeshProUGUI>();
+    }
+    void Update()
+    {
+        if (InputHelper.Player.GetButtonDown("UIConfirm")&&_textContainer.activeInHierarchy){ 
+            Continue();
+        }
+    }
+    void Continue(){
+        Time.timeScale = 1f;
+        _acp.PlaySound("Menu_Item_Select");
+        _textContainer.SetActive(false);
+        CursorSetter.LockCursor();
+    }
+    //Idea 1, pull up a screen that pauses the game
+    void PopUpTutorial(string text)
+    {
+        Time.timeScale = 0f;
+        _acp.PlaySound("Pause");
+        _textContainer.SetActive(true);
+        _text.text = text;
+        CursorSetter.UnlockCursor();
+    }
+}
---
+++ OilandCordite/Assets/_Main/Scripts/UI/TutorialTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialTrigger : GameEventUserObject
+{
+    [SerializeField] private string _text;
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag(Tags.PLAYER))
+        {
+            EventManager.Instance.TriggerEvent(new Events.TutorialArgs(_text));
+            gameObject.SetActive(false);
+        }
+    }
+}
