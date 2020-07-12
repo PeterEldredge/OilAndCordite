c9e94ab55481b840f3b952a6efb0de3320c2e03b
PeterEldredge <peldredge98@gmail.com>
2020-07-12 12:40:51 -0400

    Heat Warning UI

--- OilandCordite/Assets/_Main/Scripts/UI/HeatUIController.cs
+++ OilandCordite/Assets/_Main/Scripts/UI/HeatUIController.cs
@@ -1,21 +1,27 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class HeatUIController : BaseUIController
 {
     [SerializeField] private GameObject _heatBar;
     [SerializeField] private GameObject _heatSlider;
     [SerializeField] private GameObject _heatMask;
+    [SerializeField] private TMP_Text _warningText;
     [SerializeField] private float _minMaskSize;
     [SerializeField] private float _maxMaskSize;
+    [SerializeField] private float _warningThreshold;
+    [SerializeField] private AnimationCurve _warningAlphaAnimaiton;
 
     private Slider _sliderComp;
     private RectTransform _maskTransform;
 
     private float _maskRange;
     private float _maskY;
+
+    private float _warningAlphaTime;
+    private bool _warningAnimationRunning = false;
 
     private void Awake()
     {
@@ -24,6 +30,9 @@ public class HeatUIController : BaseUIController
         _sliderComp = _heatSlider.GetComponent<Slider>();
         _maskTransform = _heatMask.GetComponent<RectTransform>();
         _maskY = _maskTransform.sizeDelta.y;
+
+        _warningAlphaTime = _warningAlphaAnimaiton.keys[_warningAlphaAnimaiton.length - 1].time;
+        _warningText.alpha = 0f;
     }
 
     private void Update()
@@ -37,5 +46,27 @@ public class HeatUIController : BaseUIController
         _heatSlider.transform.parent = _heatMask.transform;
 
         _sliderComp.value = PlayerData.Instance.Heat;
+
+        if (PlayerData.Instance.Heat >= _warningThreshold && !_warningAnimationRunning) StartCoroutine(WarningRoutine());
+    }
+
+    private IEnumerator WarningRoutine()
+    {
+        _warningAnimationRunning = true;
+
+        float warningUptime = 0f;
+
+        while(PlayerData.Instance.Heat >= _warningThreshold)
+        {
+            warningUptime = (warningUptime + Time.deltaTime * (1 + (PlayerData.Instance.IsOverHeated ? 1 : 0))) % _warningAlphaTime;
+
+            _warningText.alpha = _warningAlphaAnimaiton.Evaluate(warningUptime);
+
+            yield return null;
+        }
+
+        _warningText.alpha = 0f;
+
+        _warningAnimationRunning = false;
     }
 }
