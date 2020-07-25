abeb0026a129ac0b059b51e3e8f9d280ca630972
JakeMcKenna1025 <jakemckenna1025@gmail.com>
2020-07-24 20:42:36 -0400

    (OAC 179) added shattered bot, added explosion test script, added test scene

---
+++ OilandCordite/Assets/_Main/JakeM/Destructable Stuff/ExplodeTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeTest : MonoBehaviour
+{
+    [SerializeField]
+    private float despawnDelay, minForce, maxForce, radius;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Explode();
+    }
+
+
+    public void Explode()
+    {
+        foreach (Transform t in transform)
+        {
+            var rb = t.GetComponent<Rigidbody>();
+
+            if(rb != null)
+            {
+                rb.AddExplosionForce(Random.Range(minForce, maxForce), transform.position, radius);
+                //t.GetComponent<MeshCollider>().enabled = false; DISABLE MESH COLLIDER ON PIECES
+            }
+
+            Destroy(t.gameObject, despawnDelay);
+        }
+        
+    }
+}
