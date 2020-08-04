15ac4c016ceab5bc8aedbbf73cecbcadf2f1077e
Jake1025 <50057517+Jake1025@users.noreply.github.com>
2020-08-03 22:22:40 -0400

    (OAC 190) work on concentric maze design. Potentially an issue with the ship being invisible, null refernece error on all enemies

---
+++ OilandCordite/Assets/_Main/JakeM/Scripts/Rotator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    [Range(-1, 1)]
+    private int direction;
+
+    [SerializeField]
+    private Vector3 rotation;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate( rotation * speed * direction * Time.deltaTime);
+    }
+}
