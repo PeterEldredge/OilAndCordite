cf5aee4ab6d94648913bd66c5c923340d6564ec5
djjbanx <jrh395@drexel.edu>
2020-08-03 23:23:13 -0400

    Add Initial test Destructible Wall
    Add folder Jack
    Add desctructible wall prefab and script
    Add a copy of the tutorial scene with the destructible wall at start

---
+++ OilandCordite/Assets/_Main/jack/DestructibleWall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleWall : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject originalObject, fracturedObject;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag(Tags.PLAYER))
+        {
+            originalObject.SetActive(false);
+            fracturedObject.SetActive(true);
+            
+            //for (int i = 0; i < fracturedObject.transform.childCount; i++)
+            //{
+            //    fracturedObject.transform.GetChild(i).GetComponent<Rigidbody>().AddForce(new Vector3(1, 0, 0));
+            //}
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
