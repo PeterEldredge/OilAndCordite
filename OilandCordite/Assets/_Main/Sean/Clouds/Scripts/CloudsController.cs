81ff8d8d4f708b8587398a288ca9a8fdc0f760f7
smcfadden150 <smcfadden150@gmail.com>
2020-08-04 01:22:35 -0400

    (OAC-000) Cleaned Up Cloud Editor

--- OilandCordite/Assets/_Main/Sean/Clouds/Scripts/CloudsController.cs
+++ OilandCordite/Assets/_Main/Sean/Clouds/Scripts/CloudsController.cs
@@ -7,37 +7,56 @@ using UnityEngine;
 [ExecuteInEditMode, ImageEffectAllowedInSceneView]
 public class CloudsController : MonoBehaviour
 {
+    [Header("General Shader Attributes")]
     public Shader shader;
     public Transform cloudContainer;
     private Material material;
+
+    [Header("Weather and Cloud Formation Maps")]
     public Texture3D noiseMap;
     public Texture3D detailMap;
     public Texture2D weatherMap;
     public Texture2D OffsetNoiseMap;
-    public Color upperGradient;
-    public Color lowerGradient;
-    public float internalWindSpeed;
+
     public float windSpeed;
+    private float internalWindSpeed = 0.0f;
+
+    [Header("Cloud Shape Settings")]
+    public float cloudScale = 1;
+
+    [Range(0, 1.5f)]
+    public float cloudDensity = 1;
+
+    [Range(0, 1.5f)]
+    public float detailNoiseWeight;
+    
     [Range(0, 100)]
-    public float densityOffset; 
+    public float densityOffset;
+
+    private Vector4 noiseWeight = new Vector4(1.0f, 1.0f, 1.0f, 1.0f);
+    private Vector4 decompositionWeight = new Vector4(1.0f, 1.0f, 1.0f, 1.0f);
+ 
+    [Header("Rendering / Optimization Settings")]
+    
     [Range(0, 100)]
     public int raySteps = 1;
 
     [Range(0,20)]
     public int lightSteps = 1;
 
-    public float cloudScale = 1;
-    public float cloudDensity = 1;
+    [Header("Cloud Lighting Settings")]
 
-    public float detailNoiseWeight;
-
+    [Range(-0.5f, 1.5f)]
     public float lightAbsorptionThroughCloud;
+
+    [Range(0.0f, 2.0f)]
     public float darknessFactor;
 
+    [Range(0.0f, 1.0f)]
     public float equatorIntensity = 1.0f;
-    public float skyIntensity = 1.0f;
 
-    public float tintAmount = 1.0f;
+    [Range(0.0f, 1.0f)]
+    public float skyIntensity = 1.0f;
 
     [Header("Cloud Fade Effect Settings")]
     public float maxCameraDistance;
@@ -45,8 +64,6 @@ public class CloudsController : MonoBehaviour
 
     private float currentDistanceToClouds;
     private CloudFadeImageEffect _cloudFade;
-    private Color _skyColor;
-    private Color _equatorColor;
     private Transform _main;
 
     [ImageEffectOpaque]
@@ -61,8 +78,6 @@ public class CloudsController : MonoBehaviour
         material.SetInt("_RaymarchSteps", raySteps);
         material.SetTexture("_NoiseTexture", noiseMap);
         material.SetTexture("_DecompositionTexture", detailMap);
-        material.SetVector("_CloudPeakColor", upperGradient);
-        material.SetVector("_CloudBaseColor", lowerGradient);
         material.SetFloat("_DensityOffset", densityOffset);
         material.SetTexture("_WeatherMap", weatherMap);
         material.SetFloat("_InternalNoiseSpeed", internalWindSpeed);
@@ -71,11 +86,12 @@ public class CloudsController : MonoBehaviour
         material.SetFloat("_CloudLightAbsorptionFactor", lightAbsorptionThroughCloud);
         material.SetFloat("_CloudScale", cloudScale);
         material.SetFloat("_DarknessThreshold", darknessFactor);
+        material.SetVector("noiseWeight", noiseWeight);
+        material.SetVector("decompositionWeight", decompositionWeight);
         material.SetFloat("_AmbientSky", RenderSettings.ambientIntensity);
         material.SetFloat("detailNoiseWeight", detailNoiseWeight);
         material.SetFloat("_DensityMultiplier", cloudDensity);
         material.SetInt("_lightMarchSteps", lightSteps);
-        material.SetFloat("_TintAmount", tintAmount);
         material.SetFloat("_SkyIntensity", skyIntensity);
         material.SetFloat("_EquatorIntensity", equatorIntensity);
 
@@ -92,18 +108,10 @@ public class CloudsController : MonoBehaviour
     {
         if(Application.isPlaying) 
         {
-            if (_skyColor == null)
-            {
-                _skyColor = RenderSettings.ambientSkyColor;
-            }
-
-            if (_equatorColor == null)
-            {
-                _equatorColor = RenderSettings.ambientEquatorColor;
-            }
-
             float distanceToCloudVolume = Vector3.Distance(_main.position, cloudContainer.position);
-            Debug.Log(distanceToCloudVolume);
+            
+            // Uncomment to help assist with determining distance to volume
+            // Debug.Log(distanceToCloudVolume);
 
             if (distanceToCloudVolume < maxCameraDistance && distanceToCloudVolume > minCameraDistance)
             {
