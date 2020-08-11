06a8dfac3bbbe2661f3a717a18ed5720a80deb17
djjbanx <jrh395@drexel.edu>
2020-08-11 01:02:23 -0400

    Add Initial Camera Orbit object Add camera script to pan and orbit Change scale of ship to 3 to make more visible Disable Enemy Targeting UI's Change health to infinite (This can be changed back for better players) Change default air speed to 120 (This can be changed back for better players)

---
+++ OilandCordite/Assets/tempCamMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tempCamMove : MonoBehaviour
+{
+
+    public GameObject target;
+    public float temp = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        temp -= 0.005f;
+        //transform.position += new Vector3(-0.1f, 0, 0);
+        transform.RotateAround(target.transform.position, Vector3.up, 5 * Time.deltaTime);
+        transform.RotateAround(target.transform.position, Vector3.right*temp, 1 * Time.deltaTime);
+
+        if (temp < -1) temp = 1;
+    }
+}
