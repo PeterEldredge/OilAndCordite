89a5914fc8c2bb584118a43da980c54280cd6806
Jake1025 <50057517+Jake1025@users.noreply.github.com>
2020-08-17 23:48:04 -0400

    (OAC 209 & Misc) changes to destructible script, changes to level 4 scale scene, checkpoint generator updates

--- OilandCordite/Assets/_Main/jack/DestructibleWall.cs
+++ OilandCordite/Assets/_Main/jack/DestructibleWall.cs
@@ -7,23 +7,20 @@ public class DestructibleWall : MonoBehaviour
     [SerializeField]
     private GameObject originalObject, fracturedObject;
 
-    void OnTriggerEnter(Collider collider)
+    [SerializeField]
+    private bool useTrigger;
+
+    private void OnTriggerEnter(Collider collider)
     {
-        if (collider.CompareTag(Tags.PLAYER))
+        if (collider.CompareTag(Tags.PLAYER) && useTrigger)
         {
-            originalObject.SetActive(false);
-            fracturedObject.SetActive(true);
-            
-            //for (int i = 0; i < fracturedObject.transform.childCount; i++)
-            //{
-            //    fracturedObject.transform.GetChild(i).GetComponent<Rigidbody>().AddForce(new Vector3(1, 0, 0));
-            //}
+            SwapObjects();
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SwapObjects()
     {
-        
+        fracturedObject.SetActive(true);
+        originalObject.SetActive(false);
     }
 }
