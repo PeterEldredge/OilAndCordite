675dac79b95321773fc52d4f1e52400ff6d54f87
Jake1025 <50057517+Jake1025@users.noreply.github.com>
2020-08-17 23:45:17 -0400

    (OAC 218 & Misc) Updates to particle systems, added new system for generator explosion

---
+++ OilandCordite/Assets/_Main/JakeM/Scripts/SpawnParticleSystem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnParticleSystem : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject particles;
+
+    public void SpawnParticles()
+    {
+        Instantiate(particles, transform.position, Quaternion.identity);
+    }
+}
