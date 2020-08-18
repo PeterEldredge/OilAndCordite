3cec570196130ce1f6d853879c6f5e0f86ac4947
dixeyja <39249054+dixeyja@users.noreply.github.com>
2020-08-18 01:59:40 -0400

    Camera Shake In progress

---
+++ OilandCordite/Assets/_Main/Jake/Scripts/CameraShake.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CameraShake : GameEventUserObject
+{
+    [SerializeField] float _duration = 1f;
+    [SerializeField] float _magnitude = 1f;
+
+    private void OnEnemyDefeated(Events.PlayerDefeatedEnemyEventArgs args) => ScreenShake();
+    private void OnObstacleHit(Events.ObstacleHitEventArgs args) => ScreenShake();
+    private FlightCam _flightCam;
+
+    public override void Subscribe()
+    {
+        EventManager.Instance.AddListener<Events.PlayerDefeatedEnemyEventArgs>(this, OnEnemyDefeated);
+        EventManager.Instance.AddListener<Events.ObstacleHitEventArgs>(this, OnObstacleHit);
+    }
+
+    private void ScreenShake()
+    {
+        transform.gameObject.GetComponent<Camera>().DOShakePosition(1f);
+    }
+}
