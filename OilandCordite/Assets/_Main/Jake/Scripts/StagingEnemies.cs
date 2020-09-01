23b88303462198e282d55d946eb876b1fc0993bf
dixeyja <39249054+dixeyja@users.noreply.github.com>
2020-09-01 04:23:45 -0400

    (AOC - 238 & 242)

---
+++ OilandCordite/Assets/_Main/Jake/Scripts/StagingEnemies.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StagingEnemies : MonoBehaviour
+{
+    [SerializeField] private float _noiseTimeOffset = .5f;
+    [SerializeField] private float _checkRate = .5f;
+    [SerializeField] GameObject _enemyGroup;
+
+    private AudioCuePlayer _acp;
+
+    private void Awake()
+    {
+        _acp = GetComponent<AudioCuePlayer>();
+        StartCoroutine(CheckEnemies());
+    }
+
+    private IEnumerator CheckEnemies()
+    {
+        while (true)
+        {
+            if (_enemyGroup.transform.childCount == 0)
+            {
+                _acp.PlayRandomSound("ShieldDown");
+                yield return new WaitForSeconds(_noiseTimeOffset);
+                gameObject.SetActive(false);
+                yield return null;
+            }
+            yield return new WaitForSeconds(_checkRate);
+        }
+    }
+}
